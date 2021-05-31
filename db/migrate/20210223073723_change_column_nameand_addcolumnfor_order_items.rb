5bb1d3d43354e7b5323fdf6d6d3a2a939d115783
balajipvr <balaji.k@pvrcinemas.com>
2021-05-31 20:44:21 +0530

    Duplicate migration file

--- db/migrate/20210223073723_change_column_nameand_addcolumnfor_order_items.rb
+++
@@ -1,7 +0,0 @@
-class ChangeColumnNameandAddcolumnforOrderItems < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :order_items, :qty, :item_qty
-    add_column :order_items, :item_unit_qty, :int
-    add_column :order_items, :item_price, :int
-  end
-end
